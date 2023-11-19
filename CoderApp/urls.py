c61d8fba4128bc7a927e9a08dc790b8217c5a1aa
Fernanda Danieli <ferdanieli.danieli@gmail.com>
2023-11-18 23:10:06 -0300

    guardar cambios

--- CoderApp/urls.py
+++ CoderApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from CoderApp.views import show_html, crear_curso
+from CoderApp.views import show_html, crear_curso, mostrar_cursos
 
 urlpatterns = [
     path('show/', show_html),
     path('crear_curso/', crear_curso),
+    path('mostrar_cursos/', mostrar_cursos),
 ]
