97d598bad2a931ff713a0739bad1a4e40bc45c15
Fernanda Danieli <ferdanieli.danieli@gmail.com>
2023-11-22 22:54:47 -0300

    buscando formularios

--- CoderApp/urls.py
+++ CoderApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from CoderApp.views import show_html, crear_curso, mostrar_cursos
+from CoderApp.views import show_html, crear_curso, mostrar_cursos, crear_curso_form
 
 urlpatterns = [
     path('show/', show_html),
     path('crear_curso/', crear_curso),
+    path('curso/', crear_curso_form),
     path('cursos/', mostrar_cursos),
 ]
--- CoderApp/views.py
+++ CoderApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from CoderApp.models import Curso
+from CoderApp.forms import CursoForm
+
 
 def mostrar_cursos(request):
     cursos = Curso.objects.all()
@@ -9,13 +11,33 @@ def mostrar_cursos(request):
     }
     return render(request, 'CoderApp/cursos.html', contexto)
 
+
 def crear_curso(request):
     curso = Curso(nombre="Java", camada=65001)
     curso.save()
 
-    return redirect("/CoderApp/cursos/") # get
+    return redirect("/CoderApp/cursos/")  # get
+
 
 def show_html(request):
     curso = Curso.objects.first()
     contexto = {"curso": curso, "nombre": "Fer"}
     return render(request, 'index.html', contexto)
+
+
+def crear_curso_form(request):
+    if request.method == "POST":
+        curso_formulario = CursoForm(request.POST)
+
+        if curso_formulario.is_valid():
+            informacion = curso_formulario.cleaned_data
+            curso_crear = Curso(nombre=informacion["nombre"], camada=informacion["camada"])
+            curso_crear.save()
+            return redirect("/CoderApp/cursos")
+
+    curso_formulario = CursoForm()
+    contexto = {
+        "form": curso_formulario
+    }
+
+    return render(request, "CoderApp/crear_curso.html", contexto)
