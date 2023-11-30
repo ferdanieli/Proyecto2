232b83f63ee3e2e7da848924ef90060e5b7ffde4
Fernanda Danieli <ferdanieli.danieli@gmail.com>
2023-11-30 18:21:01 -0300

    clases basadas en vista

---
+++ accounts/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            usuario = data.get('username')
+            contrasenia = data.get('password')
+
+            user = authenticate(username=usuario, password=contrasenia)
+
+            if user:
+                login(request, user)
+
+
+        return redirect('/CoderApp/Curso/list')
+
+    form = AuthenticationForm()
+    contexto = {
+        "form": form
+    }
+    return render(request, "accounts/login.html", contexto)
